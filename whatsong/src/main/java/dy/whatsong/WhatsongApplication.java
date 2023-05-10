510950242ea3162fff9a46a816fa4477150997a2
seonghoo1217 <seonghoo1217@naver.com>
2023-05-10 21:06:43 +0900

    #2 chore : add dependency and project basic setting

---
+++ whatsong/src/main/java/dy/whatsong/WhatsongApplication.java
@@ -0,0 +1,13 @@
+package dy.whatsong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsongApplication.class, args);
+	}
+
+}
---
+++ whatsong/src/test/java/dy/whatsong/WhatsongApplicationTests.java
@@ -0,0 +1,13 @@
+package dy.whatsong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
