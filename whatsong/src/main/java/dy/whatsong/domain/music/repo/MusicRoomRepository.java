0ed7c00b650902744603b64cfa84f019351b0819
seonghoo1217 <seonghoo1217@naver.com>
2023-05-24 21:16:16 +0900

    Revert "#2 feat : add MusicRoom Domain JPA Repo"
    This reverts commit 0c995b49aa0128bd137904ae2ca695f4ea79741c.

--- whatsong/src/main/java/dy/whatsong/domain/music/repo/MusicRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package dy.whatsong.domain.music.repo;
-
-import dy.whatsong.domain.music.entity.MusicRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MusicRoomRepository extends JpaRepository<MusicRoom,Long> {
-}
