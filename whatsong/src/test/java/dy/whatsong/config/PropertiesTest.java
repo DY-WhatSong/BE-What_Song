736185036d1ab5f1e68bc405be3020c1b097606f
seonghoo1217 <seonghoo1217@naver.com>
2023-07-11 22:20:38 +0900

    #2 test : application correct load the profile info

---
+++ whatsong/src/test/java/dy/whatsong/config/PropertiesTest.java
@@ -0,0 +1,22 @@
+package dy.whatsong.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
+
+@SpringBootTest
+public class PropertiesTest {
+
+	@Autowired
+	ApplicationContext ctx;
+
+	@Test
+	public void getPropertyInfo(){
+		Environment environment = ctx.getEnvironment();
+		System.out.println("Log="+Arrays.toString(environment.getActiveProfiles()));
+	}
+}
