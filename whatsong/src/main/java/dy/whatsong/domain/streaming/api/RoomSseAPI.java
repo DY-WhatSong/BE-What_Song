1a6c3a84db337a7617160c8a148dd06ccb14fe60
seonghoo1217 <seonghoo1217@naver.com>
2023-08-10 23:50:21 +0900

    #26 refactor : eliminate sse api

--- whatsong/src/main/java/dy/whatsong/domain/streaming/api/RoomSseAPI.java
+++ whatsong/src/main/java/dy/whatsong/domain/streaming/api/RoomSseAPI.java
@@ -9,7 +9,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.SignalType;
 
+import java.time.Duration;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -20,27 +22,4 @@ public class RoomSseAPI {
 
     private final RoomSseService roomSseService;
 
-    private final Map<String,Flux<MRSseResponse>> sseStreamToRoom=new ConcurrentHashMap<>();
-
-    /*@GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<ServerSentEvent<MRSseResponse>> connectionRoomSSE(@PathVariable String roomCode){
-        return sseStreamToRoom.computeIfAbsent(roomCode,code->{
-            return Flux.interval(Duration.ofSeconds(1))
-                    .map(seq->{
-                        MemberRe
-                    })
-        })
-    }*/
-
-    @PostMapping
-    public Flux<ServerSentEvent<MRSseResponse>> sendCurrentRoomState(@PathVariable("roomCode")String roomCode,@RequestBody MRSseRequest.playerCurrentState playerCurrentState){
-        MRSse findMRSse = roomSseService.getMRSseByRoomCode(roomCode);
-        return Flux.just(MRSseResponse.builder()
-                            .currentTime(playerCurrentState.getTimeStamp())
-                            .roomCode(roomCode)
-                            .status(findMRSse.getStatus())
-                            .videoId(findMRSse.getVideoId())
-                            .build())
-                .map(MRSSeRes->ServerSentEvent.builder(MRSSeRes).build());
-    }
 }
