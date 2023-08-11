e816accf0dd4b85817445286c42d6fd8419dae7f
seonghoo1217 <seonghoo1217@naver.com>
2023-08-11 16:39:42 +0900

    #26 refactor : modify originally ws config class

--- whatsong/src/main/java/dy/whatsong/global/config/ws/WebSocketConfig.java
+++ whatsong/src/main/java/dy/whatsong/global/config/ws/WebSocketConfig.java
@@ -1,14 +1,22 @@
 package dy.whatsong.global.config.ws;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
-	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(new SocketTextHandler(), "/user")
-				.setAllowedOrigins("*")
-				.withSockJS();
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/sub");
+		config.setApplicationDestinationPrefixes("/pub");
+	}
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/stomp").setAllowedOrigins("http://localhost:3000").withSockJS();
 	}
 }
