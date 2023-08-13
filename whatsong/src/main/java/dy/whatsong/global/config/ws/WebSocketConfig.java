eeb87a05c1c8ef24c5a25b2815595699ecc0fa4a
seonghoo1217 <seonghoo1217@naver.com>
2023-08-13 22:00:54 +0900

    #26 refactor : modify cross origin setting

--- whatsong/src/main/java/dy/whatsong/global/config/ws/WebSocketConfig.java
+++ whatsong/src/main/java/dy/whatsong/global/config/ws/WebSocketConfig.java
@@ -17,6 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/stomp").setAllowedOrigins("http://localhost:3000").withSockJS();
+		registry.addEndpoint("/stomp").setAllowedOrigins("**").withSockJS();
 	}
 }
