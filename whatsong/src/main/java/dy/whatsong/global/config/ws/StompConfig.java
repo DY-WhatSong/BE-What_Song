5dc5222d010743ae1c5d687bf6924815cd587022
seonghoo1217 <seonghoo1217@naver.com>
2023-08-22 00:37:55 +0900

    #26 feat : separate subscribe endpoint for purpose use case and add annotation

--- whatsong/src/main/java/dy/whatsong/global/config/ws/StompConfig.java
+++ whatsong/src/main/java/dy/whatsong/global/config/ws/StompConfig.java
@@ -16,10 +16,19 @@ public class StompConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*").withSockJS();
     }
 
+
+    /**
+     * WS(WebSocket) Config Broker
+     * Subscribe Spec Annotation
+     *
+     * @ topic : 1:1 Message 구독
+     * @ queue : 1:N Message 구독
+     * @ stream : 뮤직 플레이어 관련 구독
+     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic","/queue")
+        registry.enableSimpleBroker("/topic","/queue","stream")
                 .setTaskScheduler(taskScheduler())
                 .setHeartbeatValue(new long[] {3000L, 3000L});
     }
