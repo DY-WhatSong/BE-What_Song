77ddd88b2480e22918a122bb02a68578211b5a28
seonghoo1217 <seonghoo1217@naver.com>
2023-09-07 18:35:02 +0900

    #26 feat : add stomp interceptor when user enter

--- whatsong/src/main/java/dy/whatsong/global/config/ws/StompConfig.java
+++ whatsong/src/main/java/dy/whatsong/global/config/ws/StompConfig.java
@@ -1,12 +1,27 @@
 package dy.whatsong.global.config.ws;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -37,5 +52,31 @@ public class StompConfig implements WebSocketMessageBrokerConfigurer {
         ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
         taskScheduler.initialize();
         return taskScheduler;
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    String user = accessor.getFirstNativeHeader("user");
+                    System.out.println("User="+user);
+                    if (user != null) {
+                        List<GrantedAuthority> authorities = new ArrayList<>();
+                        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+                        Authentication auth = new UsernamePasswordAuthenticationToken(user, user, authorities);
+                        SecurityContextHolder.getContext().setAuthentication(auth);
+                        System.out.println(auth.toString());
+                        accessor.setUser(auth);
+                    }
+                }
+
+                return message;
+            }
+        });
     }
 }
