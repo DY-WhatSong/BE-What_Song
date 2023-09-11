499eeb1a1f9ad131fe330d6aef8e006f11ca52e4
seonghoo1217 <seonghoo1217@naver.com>
2023-09-11 23:20:28 +0900

    #26 feat : 스프링캐시 사용부분을 단순 RestAPI에서 Stomp Connect시 연결

---
+++ whatsong/src/main/java/dy/whatsong/domain/streaming/application/impl/StompServiceImpl.java
@@ -0,0 +1,6 @@
+package dy.whatsong.domain.streaming.application.impl;
+
+import dy.whatsong.domain.streaming.application.service.StompService;
+
+public class StompServiceImpl implements StompService {
+}
---
+++ whatsong/src/main/java/dy/whatsong/domain/streaming/application/service/StompService.java
@@ -0,0 +1,5 @@
+package dy.whatsong.domain.streaming.application.service;
+
+public interface StompService {
+    void memberEnterTheRoom(String token);
+}
