395a25ac6317b175b0ae7720a2c37eb806d9f344
seonghoo1217 <seonghoo1217@naver.com>
2023-09-16 09:08:40 +0900

    #26 refactor : modify dto class in use caching logic

--- whatsong/src/main/java/dy/whatsong/domain/member/dto/MemberRequestCacheDTO.java
+++ whatsong/src/main/java/dy/whatsong/domain/member/dto/MemberRequestCacheDTO.java
@@ -1,13 +1,26 @@
 package dy.whatsong.domain.member.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class MemberRequestCacheDTO {
 
+    @Getter
+    public static class PutOrModify{
+        private String roomCode;
+        private Long memberSeq;
+    }
 
     @Getter
     public static class OnlyUseRoomCode{
         private String roomCode;
+    }
+
+    @Getter
+    @Builder
+    public static class BasicInfo{
+        private String roomCode;
+        private String username;
     }
 }
