4c15aaafd8233d27f7df92b1b5d0d4a796792a19
seonghoo1217 <seonghoo1217@naver.com>
2023-10-19 00:53:44 +0900

    #43 refactor : modify story domain field for use case

---
+++ whatsong/src/main/java/dy/whatsong/domain/story/dto/StoryVideo.java
@@ -0,0 +1,11 @@
+package dy.whatsong.domain.story.dto;
+
+import lombok.Getter;
+
+@Getter
+public class StoryVideo {
+    private String videoId;
+    private String title;
+    private String channelName;
+    private String thumbnailUrl;
+}
--- whatsong/src/main/java/dy/whatsong/domain/story/entity/Story.java
+++ whatsong/src/main/java/dy/whatsong/domain/story/entity/Story.java
@@ -1,6 +1,8 @@
 package dy.whatsong.domain.story.entity;
 
 import dy.whatsong.domain.member.dto.MemberDto;
+import dy.whatsong.domain.story.dto.StoryVideo;
+import dy.whatsong.domain.youtube.dto.StoryVideoReq;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
@@ -17,4 +19,8 @@ public class Story {
     private String id;
     private MemberDto.MemberStory memberStory;
     private LocalDateTime localDateTime;
+    private String img_url;
+    private String start;
+    private String end;
+    private StoryVideo storyVideo;
 }
