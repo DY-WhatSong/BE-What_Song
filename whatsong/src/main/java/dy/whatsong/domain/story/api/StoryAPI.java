f6250dd4a30f38d7c9e7609e55d4ef156c7131df
seonghoo1217 <seonghoo1217@naver.com>
2023-10-25 21:23:03 +0900

    #43 feat : add post story api

---
+++ whatsong/src/main/java/dy/whatsong/domain/story/api/StoryAPI.java
@@ -0,0 +1,24 @@
+package dy.whatsong.domain.story.api;
+
+
+import dy.whatsong.domain.story.application.service.StoryService;
+import dy.whatsong.domain.story.dto.req.StoryPostReq;
+import dy.whatsong.domain.story.entity.Story;
+import dy.whatsong.global.annotation.EssentialController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@EssentialController
+@RequiredArgsConstructor
+public class StoryAPI {
+    private final StoryService storyService;
+
+    @PostMapping("/story")
+    public ResponseEntity<?> postStoryByMember(@RequestBody StoryPostReq storyPostReq){
+        Story postedStory = storyService.memberPostStory(storyPostReq);
+        return new ResponseEntity<>(postedStory, HttpStatus.OK);
+    }
+}
