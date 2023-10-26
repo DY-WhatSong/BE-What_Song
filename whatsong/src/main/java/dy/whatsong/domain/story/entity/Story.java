2869abb62f770d770589814be6e2d32bbb77b98a
seonghoo1217 <seonghoo1217@naver.com>
2023-10-26 18:21:39 +0900

    #43 refactor : add redis hash value

--- whatsong/src/main/java/dy/whatsong/domain/story/entity/Story.java
+++ whatsong/src/main/java/dy/whatsong/domain/story/entity/Story.java
@@ -1,5 +1,7 @@
 package dy.whatsong.domain.story.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import dy.whatsong.domain.member.dto.MemberDto;
 import dy.whatsong.domain.story.dto.StoryVideo;
 import lombok.Builder;
@@ -10,7 +12,7 @@ import org.springframework.data.redis.core.RedisHash;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-@RedisHash(timeToLive = 86400)
+@RedisHash(timeToLive = 86400,value = "story")
 @Getter
 @Builder
 public class Story implements Serializable {
@@ -18,6 +20,7 @@ public class Story implements Serializable {
     @Id
     private String id;
     private MemberDto.MemberStory memberStory;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime postTime;
     private String img_url;
     private String start;
