a6122aba8a272cff845487e1e2151e739d30fc9a
seonghoo1217 <seonghoo1217@naver.com>
2023-11-12 01:05:11 +0900

    #43 refactor : modify room socket reservation list in approve

--- whatsong/src/main/java/dy/whatsong/domain/reservation/application/impl/ReservationServiceImpl.java
+++ whatsong/src/main/java/dy/whatsong/domain/reservation/application/impl/ReservationServiceImpl.java
@@ -24,96 +24,95 @@ import java.util.stream.StreamSupport;
 @Log4j2
 public class ReservationServiceImpl implements ReservationService {
 
-	private final JPAQueryFactory jpaQueryFactory;
+    private final JPAQueryFactory jpaQueryFactory;
 
-	private final ReservationRepository reservationRepository;
+    private final ReservationRepository reservationRepository;
 
-	private final MusicMemberService musicMemberService;
+    private final MusicMemberService musicMemberService;
 
-	private final RoomWsService roomWsService;
+    private final RoomWsService roomWsService;
 
-	@Override
-	public ResponseEntity<?> reservationMusic(ReservationDTO.Select selectDTO) {
-		System.out.println("대기열 등록");
-		Reservation currentReservation = reservationRepository.save(Reservation.builder()
-				.reservationId(UUID.randomUUID().toString())
-				.roomSeq(selectDTO.getRoomSeq())
-				.selectVideo(ReservationDTO.SelectVideo
-						.builder()
-						.videoId(selectDTO.getVideoId())
-						.channelName(selectDTO.getChannelName())
-						.thumbnailUrl(selectDTO.getThumbnailUrl())
-						.title(selectDTO.getTitle())
-						.build())
-				.recognize(Recognize.NONE)
-				.build());
-		if (musicMemberService.memberIsRoomOwner(selectDTO.getMemberSeq(), selectDTO.getRoomSeq())){
-			approveReservation(new ReservationDTO.Approve(currentReservation.getReservationId(),Recognize.APPROVE));
-		}
-		return new ResponseEntity<>(currentReservation, HttpStatus.OK);
-	}
+    @Override
+    public ResponseEntity<?> reservationMusic(ReservationDTO.Select selectDTO) {
+        System.out.println("대기열 등록");
+        Reservation currentReservation = reservationRepository.save(Reservation.builder()
+                .reservationId(UUID.randomUUID().toString())
+                .roomSeq(selectDTO.getRoomSeq())
+                .selectVideo(ReservationDTO.SelectVideo
+                        .builder()
+                        .videoId(selectDTO.getVideoId())
+                        .channelName(selectDTO.getChannelName())
+                        .thumbnailUrl(selectDTO.getThumbnailUrl())
+                        .title(selectDTO.getTitle())
+                        .build())
+                .recognize(Recognize.NONE)
+                .build());
+        if (musicMemberService.memberIsRoomOwner(selectDTO.getMemberSeq(), selectDTO.getRoomSeq())) {
+            approveReservation(new ReservationDTO.Approve(currentReservation.getReservationId(), Recognize.APPROVE));
+        }
+        return new ResponseEntity<>(currentReservation, HttpStatus.OK);
+    }
 
-	@Override
-	public List<Reservation> reservationList(Long roomSeq) {
-		System.out.println("roomSeq="+roomSeq);
-		List<Reservation> reservations = StreamSupport.stream(reservationRepository.findAll().spliterator(), false)
-				.filter(Objects::nonNull)
-				.filter(reservation -> reservation.getRoomSeq().equals(roomSeq) && reservation.getRecognize().equals(Recognize.NONE))
-				.collect(Collectors.toList());
+    @Override
+    public List<Reservation> reservationList(Long roomSeq) {
+        System.out.println("roomSeq=" + roomSeq);
+        List<Reservation> reservations = StreamSupport.stream(reservationRepository.findAll().spliterator(), false)
+                .filter(Objects::nonNull)
+                .filter(reservation -> reservation.getRoomSeq().equals(roomSeq) && reservation.getRecognize().equals(Recognize.NONE))
+                .collect(Collectors.toList());
+        return reservations;
+    }
 
-		return Optional.of(reservations).orElse(new ArrayList<>());
-	}
+    @Override
+    public List<Reservation> approveReservationList(Long roomSeq) {
+        List<Reservation> reservations = StreamSupport.stream(reservationRepository.findAll().spliterator(), false)
+                .filter(Objects::nonNull)
+                .filter(reservation -> reservation.getRoomSeq().equals(roomSeq) && reservation.getRecognize().equals(Recognize.APPROVE))
+                .collect(Collectors.toList());
 
-	@Override
-	public List<Reservation> approveReservationList(Long roomSeq) {
-		List<Reservation> reservations = StreamSupport.stream(reservationRepository.findAll().spliterator(), false)
-				.filter(Objects::nonNull)
-				.filter(reservation -> reservation.getRoomSeq().equals(roomSeq) && reservation.getRecognize().equals(Recognize.APPROVE))
-				.collect(Collectors.toList());
-
-		return Optional.of(reservations).orElse(new ArrayList<>());
-	}
-
+        return Optional.of(reservations).orElse(new ArrayList<>());
+    }
 
 
-	/**
-		승인 뿐 아니라 거절 되면 리스트 삭제 또한 구현되어야함
+    /**
+     * 승인 뿐 아니라 거절 되면 리스트 삭제 또한 구현되어야함
+     * <p>
+     * 이로 인해 승인리스트와 대기열 리스트를 별도의 도메인으로 분류해야한다고 판단
+     */
+    @Override
+    public ResponseEntity<?> approveReservation(final ReservationDTO.Approve approveDTO) {
+        System.out.println("뮤직승인!");
+        Optional<Reservation> findOptionReservation = reservationRepository.findById(approveDTO.getReservationId());
+        if (findOptionReservation.isEmpty()) {
+            throw new InvalidRequestAPIException("Invalid Request", 400);
+        }
+        Reservation reSaveReserv = reSaveReservationEntity(
+                findOptionReservation.get()
+                , approveDTO.getRecognize());
+        return new ResponseEntity<>(
+                roomWsService.getCurrentReservationList(reSaveReserv)
+                , HttpStatus.OK);
+    }
 
-	 	이로 인해 승인리스트와 대기열 리스트를 별도의 도메인으로 분류해야한다고 판단
-	*/
-	@Override
-	public ResponseEntity<?> approveReservation(final ReservationDTO.Approve approveDTO) {
-		Optional<Reservation> findOptionReservation = reservationRepository.findById(approveDTO.getReservationId());
-		if (findOptionReservation.isEmpty()){
-			throw new InvalidRequestAPIException("Invalid Request",400);
-		}
-		Reservation reSaveReserv = reSaveReservationEntity(
-				findOptionReservation.get()
-				, approveDTO.getRecognize());
-		return new ResponseEntity<>(
-					roomWsService.getCurrentReservationList(reSaveReserv)
-					,HttpStatus.OK);
-	}
+    @Override
+    public ResponseEntity<?> musicSkipByController(Long roomSeq) {
+        return null;
+    }
 
-	@Override
-	public ResponseEntity<?> musicSkipByController(Long roomSeq) {
-		return null;
-	}
+    private Reservation reSaveReservationEntity(final Reservation reservation, final Recognize changeRecognize) {
+        Reservation changeReserEntity = Reservation.builder()
+                .reservationId(reservation.getReservationId())
+                .selectVideo(reservation.getSelectVideo())
+                .recognize(changeRecognize)
+                .roomSeq(reservation.getRoomSeq())
+                .build();
 
-	private Reservation reSaveReservationEntity(final Reservation reservation,final Recognize changeRecognize){
-		Reservation changeReserEntity = Reservation.builder()
-				.reservationId(reservation.getReservationId())
-				.selectVideo(reservation.getSelectVideo())
-				.recognize(changeRecognize)
-				.roomSeq(reservation.getRoomSeq())
-				.build();
+        return reservationRepository.save(changeReserEntity);
+    }
 
-		return reservationRepository.save(changeReserEntity);
-	}
-
-	@Override
-	public List<Reservation> reservationReject(Long roomSeq,String reservationId){
-		reservationRepository.deleteById(reservationId);
-		return reservationList(roomSeq);
-	}
+    @Override
+    public List<Reservation> reservationReject(Long roomSeq, String reservationId) {
+        reservationRepository.deleteById(reservationId);
+        return reservationList(roomSeq);
+    }
 }
--- whatsong/src/main/java/dy/whatsong/domain/streaming/api/RoomSocketAPI.java
+++ whatsong/src/main/java/dy/whatsong/domain/streaming/api/RoomSocketAPI.java
@@ -7,7 +7,6 @@ import dy.whatsong.domain.streaming.application.service.StompService;
 import dy.whatsong.domain.streaming.dto.MRWSRequest;
 import dy.whatsong.domain.streaming.dto.MRWSResponse;
 import dy.whatsong.domain.streaming.entity.redis.RoomMember;
-import dy.whatsong.domain.streaming.entity.room.Status;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -37,44 +36,45 @@ public class RoomSocketAPI {
     }*/
 
     @MessageMapping("/{roomCode}/music/current/new")
-    public void currentRoomStateInfoUptoDate(@DestinationVariable String roomCode, @RequestBody MRWSRequest.updateInfo updateInfoDTO){
+    public void currentRoomStateInfoUptoDate(@DestinationVariable String roomCode, @RequestBody MRWSRequest.updateInfo updateInfoDTO) {
         System.out.println("!!!!");
-        System.out.println("status:"+updateInfoDTO.getStatus());
+        System.out.println("status:" + updateInfoDTO.getStatus());
         MRWSResponse.updateInfoRes updateInfoRes = getUpdateInfoToResDTO(updateInfoDTO);
-        template.convertAndSend("/stream/"+roomCode+"/music/current/info",updateInfoRes);
+        template.convertAndSend("/stream/" + roomCode + "/music/current/info", updateInfoRes);
     }
 
     @MessageMapping("/{roomCode}/room/enter")
-    public void memberEnterTheMusicRoom(@DestinationVariable String roomCode){
+    public void memberEnterTheMusicRoom(@DestinationVariable String roomCode) {
         RoomMember roomOfMemberList = memberCacheService.getRoomOfMemberList(roomCode);
-        template.convertAndSend("/stream/"+roomCode+"/room/enter",roomOfMemberList);
+        template.convertAndSend("/stream/" + roomCode + "/room/enter", roomOfMemberList);
     }
-    
+
     @MessageMapping("/{roomCode}/room/leave")
-    public void memberLeaveTheMusicRoom(@DestinationVariable String roomCode){
+    public void memberLeaveTheMusicRoom(@DestinationVariable String roomCode) {
         RoomMember roomOfMemberList = memberCacheService.getRoomOfMemberList(roomCode);
-        System.out.println("roomCode!!!!!!!="+roomCode);
-        template.convertAndSend("/stream/"+roomCode+"/room/leave",roomOfMemberList);
+        System.out.println("roomCode!!!!!!!=" + roomCode);
+        template.convertAndSend("/stream/" + roomCode + "/room/leave", roomOfMemberList);
     }
 
     @MessageMapping("/{roomCode}/room/member")
-    public void memberCurrentInRoom(@DestinationVariable String roomCode){
+    public void memberCurrentInRoom(@DestinationVariable String roomCode) {
         RoomMember roomOfMemberList = memberCacheService.getRoomOfMemberList(roomCode);
-        template.convertAndSend("/stream/"+roomCode+"/room/member",roomOfMemberList);
+        template.convertAndSend("/stream/" + roomCode + "/room/member", roomOfMemberList);
     }
 
     @MessageMapping("/{roomCode}/playlist/current/new")
-    public void currentRoomStateInfo(@DestinationVariable String roomCode,@RequestBody MRWSRequest.playerCurrentState playerCurrentState){
-        List<Reservation> reservationList = reservationService.reservationList(playerCurrentState.getRoomSeq());
-        if (playerCurrentState.getController()==null){
-            template.convertAndSend("/stream/"+roomCode+"/playlist/current/info",reservationList);
-        }else {
+    public void currentRoomStateInfo(@DestinationVariable String roomCode, @RequestBody MRWSRequest.playerCurrentState playerCurrentState) {
+        List<Reservation> reservationList = reservationService.approveReservationList(playerCurrentState.getRoomSeq());
+        System.out.println("제일중요=" + reservationList);
+        if (playerCurrentState.getController() == null) {
+            template.convertAndSend("/stream/" + roomCode + "/playlist/current/info", reservationList);
+        } else {
             List<Reservation> retuReservationList = stompService.musicSkipInPlaylist(reservationList);
-            template.convertAndSend("/stream/"+roomCode+"/playlist/current/info",retuReservationList);
+            template.convertAndSend("/stream/" + roomCode + "/playlist/current/info", retuReservationList);
         }
     }
 
-    public MRWSResponse.updateInfoRes getUpdateInfoToResDTO(MRWSRequest.updateInfo updateInfo){
+    public MRWSResponse.updateInfoRes getUpdateInfoToResDTO(MRWSRequest.updateInfo updateInfo) {
         return MRWSResponse.updateInfoRes.builder()
                 .username(updateInfo.getUsername())
                 .timeStamp(updateInfo.getTimeStamp())
