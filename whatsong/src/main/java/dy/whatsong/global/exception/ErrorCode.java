ac0247ae025059ad8d393a68ecafa05abf6aa650
Lee.t.c <lsh6451217@gmail.com>
2024-02-06 23:16:01 +0900

    feat: 추가 에러 처리

--- whatsong/src/main/java/dy/whatsong/global/exception/ErrorCode.java
+++ whatsong/src/main/java/dy/whatsong/global/exception/ErrorCode.java
@@ -9,8 +9,8 @@ import java.util.Set;
 public enum ErrorCode {
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 생겼습니다. 관리자에게 문의하세요.", Set.of()),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다.", Set.of(UnauthorizedException.class));
-
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다.", Set.of(UnauthorizedException.class)),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.", Set.of(InvalidRequestAPIException.class));
 
     private final HttpStatus status;
     private final String code;
